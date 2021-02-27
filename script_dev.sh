2ddfc54f56e1a7652610d82f5792019529dcf648
Qixiong Yuan <thisdk@gmail.com>
2021-02-28 04:18:35 +0800

    Update script_dev.sh

--- script_dev.sh
+++ script_dev.sh
@@ -6,6 +6,10 @@ git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
 
 cd openwrt
 
+# version replace
+
+sed -i 's/-SNAPSHOT/.1/g' include/version.mk
+
 # source update and install
 
 ./scripts/feeds update -a	
