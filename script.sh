a77471948d3449db7d990ece61dcb76f3177a8b1
Qixiong Yuan <thisdk@gmail.com>
2021-07-23 20:12:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,19 +4,9 @@
 
 git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
 
-# version replace
-
-cd openwrt && sed -i 's/-SNAPSHOT/.1/g' include/version.mk
-
 # source update and install
 
-./scripts/feeds update -a	 && ./scripts/feeds install -a
-
-# clone openwrt plugin source
-
-git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/gw826943555/openwrt-vlmcsd.git package/openwrt-vlmcsd
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 # copy build file and config
 
