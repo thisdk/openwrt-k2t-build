4f3cd2e71928c231bbe32ef79c03d900bf470c45
Qixiong Yuan <thisdk@gmail.com>
2021-08-01 15:53:04 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 # clone openwrt
 
 git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
-git clone https://github.com/kenzok8/openwrt-packages.git
 
 # version replace
 
@@ -11,15 +10,9 @@ cd openwrt && sed -i 's/-SNAPSHOT/.1/g' include/version.mk
 
 # clone openwrt plugin source
 
-./scripts/feeds update -a
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 git clone https://github.com/gw826943555/openwrt-vlmcsd.git package/openwrt-vlmcsd
-
-mv ../openwrt-packages/luci-app-smartdns/ package/
-
-mv ../openwrt-packages/smartdns/ package/
-
-./scripts/feeds install -a
 
 # copy build file and config
 
